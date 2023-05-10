20400f4f8ebf3976876816862c7478b42ef7cea0
supriyagabhane <120486976+supriyagabhane@users.noreply.github.com>
2023-05-10 14:47:29 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,18 @@
+// Getting an element from HTML file
+const wrapper = document.getElementById('wrapper');
+
+const favMovie = JSON.parse(localStorage.getItem('favMoviesArray'));
+
+function addMovieToFavouritePage() {
+    
+    for (let i = 0; i < favMovie.length; i++) {
+        let poster = favMovie[i].poster;
+        let movieCard = document.createElement('div');
+        movieCard.classList.add('movie-card');
+        movieCard.innerHTML = `<img class="poster" src=${poster}>`;
+        wrapper.appendChild(movieCard);
+    }
+
+}
+
+addMovieToFavouritePage();
